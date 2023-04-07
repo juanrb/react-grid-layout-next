fe367be3313e5628255b7835f3b1ae1fe8e072ae
Marcus Pousette <marcus.pousette@quantleaf.com>
2023-04-07 15:17:50 +0200

    update example to calculate initial width from view

--- packages/examples/src/pages/21-responsive-layout-new-breakpoints.tsx
+++ packages/examples/src/pages/21-responsive-layout-new-breakpoints.tsx
@@ -11,25 +11,11 @@ let breakpoints: Breakpoints<string> = {
 	'xxs': 0
 
 }
-const generateLayout = (items: number): Record<Breakpoint, Layout> => {
-	let layouts = {
-		md: _.map(new Array(items), (_item, i) => {
-			const y = Math.ceil(Math.random() * 4) + 1;
-			return {
-				x: (i * 2) % 12,
-				y: Math.floor(i / 6) * y,
-				w: 2,
-				h: (i + 1),
-				i: i.toString()
-			}
-		})
-	}
-	return layouts
-}
+
 
 export const ResponseLayoutNewBreakPoint = () => {
 	const [items, setItems] = useState(2)
-	const [layouts, setLayouts] = useState(() => generateLayout(items))
+	const [layouts, setLayouts] = useState<Record<string, Layout>>()
 	const breakpoint = useRef<Breakpoint>()
 
 	const generateDOM = () => {
@@ -47,23 +33,36 @@ export const ResponseLayoutNewBreakPoint = () => {
 		<div>
 			<p>Layouts are shared between breakpoints if missing. For example, you set up the layout in 'md' but you resize the window to 'sm' then the 'sm' layout will be generated as good as possible from 'md' on resize</p>
 			<button onClick={() => {
+				if (!layouts) {
+					return;
+				}
 				setItems(items + 1);
 				if (!breakpoint.current) {
 					throw new Error("Missing current breakpoint")
 				}
-				layouts[breakpoint.current].push({ h: 1, i: String(layouts[breakpoint.current].length), w: 1, x: 0, y: 0 })
+				layouts[breakpoint.current]?.push({ h: 1, i: String(layouts[breakpoint.current].length), w: 1, x: 0, y: 0 })
 				setLayouts(layouts)
 
 			}}>Add item</button>
 			<ResponsiveGridLayout
-				cols={{ md: 10, xxs: 2 }}
+				cols={{ md: 3, xxs: 2 }}
 				rowHeight={(width: number) => width}
-				layouts={layouts}
+				layout={_.map(new Array(items), (_item, i) => {
+					const y = Math.ceil(Math.random() * 4) + 1;
+					return {
+						x: (i * 2) % 12,
+						y: Math.floor(i / 6) * y,
+						w: 2,
+						h: (i + 1),
+						i: i.toString()
+					}
+				})} // initial layout
 				onLayoutChange={({ layouts }) => {
 					setLayouts(layouts)
 				}}
 				breakpoints={breakpoints}
 				useCSSTransforms={true}
+				measureBeforeMount={true}
 				onBreakpointChange={(b) => {
 					breakpoint.current = b;
 				}}
